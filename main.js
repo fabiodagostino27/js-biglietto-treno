25b00bbc33c7b5f5d71906e7795dd022311eadbe
fabiodagostino27 <fabiodago27@gmail.com>
2025-01-09 21:24:54 +0100

    Shorter code

--- main.js
+++ main.js
@@ -10,11 +10,14 @@ let discount;
 if (age < 18) {
     discount = provisionalPrice * 0.2;
     finalPrice = provisionalPrice - discount;
-    console.log(`Il prezzo del biglietto è di: ${finalPrice.toFixed(2)}€`)
+    //console.log(`Il prezzo del biglietto è di: ${finalPrice.toFixed(2)}€`)
 } else if (age > 65) {
     discount = provisionalPrice * 0.4;
     finalPrice = provisionalPrice - discount;
-    console.log(`Il prezzo del biglietto è di: ${finalPrice.toFixed(2)}€`)
+    //console.log(`Il prezzo del biglietto è di: ${finalPrice.toFixed(2)}€`)
 } else {
-    console.log(`Il prezzo del biglietto è di: ${provisionalPrice.toFixed(2)}€`)
+    finalPrice = provisionalPrice
+    //console.log(`Il prezzo del biglietto è di: ${provisionalPrice.toFixed(2)}€`)
 }
+
+console.log(`Il prezzo del biglietto è di: ${finalPrice.toFixed(2)}€`)
