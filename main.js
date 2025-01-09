f73c3fef741bb1c1a2fcd72f9e8a97a88c75caa4
fabiodagostino27 <fabiodago27@gmail.com>
2025-01-09 13:07:11 +0100

    ADD: final price calculations and discounts

--- main.js
+++ main.js
@@ -1,3 +1,18 @@
 // Richiesta km e età all'utente
-let km = parseInt(prompt("Quanti sarà lungo il suo viaggio in chilometri?"))
-let age = parseInt(prompt("Quanti anni ha?"))
+let km = parseInt(prompt("Quanti sarà lungo il suo viaggio in chilometri?"));
+let age = parseInt(prompt("Quanti anni ha?"));
+
+// Calcolo prezzo biglietto
+let provisionalPrice = km * 0.21;
+
+if(age < 18) {
+    let discount = provisionalPrice * 0.2;
+    let finalPrice = provisionalPrice - discount;
+    console.log(`Il prezzo del biglietto è di: ${finalPrice}€`)
+} else if(age > 65) {
+    let discount = provisionalPrice * 0.4;
+    let finalPrice = provisionalPrice - discount;
+    console.log(`Il prezzo del biglietto è di: ${finalPrice}€`)
+} else {
+    console.log(`Il prezzo del biglietto è di: ${provisionalPrice}€`)
+}
