4cab9f5d82d411c8e346d6fb43907343924308f9
fabiodagostino27 <fabiodago27@gmail.com>
2025-01-09 19:58:59 +0100

    ADD: variables fix

--- main.js
+++ main.js
@@ -4,14 +4,16 @@ let age = parseInt(prompt("Quanti anni ha?"));
 
 // Calcolo prezzo biglietto
 let provisionalPrice = km * 0.21;
+let finalPrice;
+let discount;
 
-if(age < 18) {
-    let discount = provisionalPrice * 0.2;
-    let finalPrice = provisionalPrice - discount;
+if (age < 18) {
+    discount = provisionalPrice * 0.2;
+    finalPrice = provisionalPrice - discount;
     console.log(`Il prezzo del biglietto è di: ${finalPrice.toFixed(2)}€`)
-} else if(age > 65) {
-    let discount = provisionalPrice * 0.4;
-    let finalPrice = provisionalPrice - discount;
+} else if (age > 65) {
+    discount = provisionalPrice * 0.4;
+    finalPrice = provisionalPrice - discount;
     console.log(`Il prezzo del biglietto è di: ${finalPrice.toFixed(2)}€`)
 } else {
     console.log(`Il prezzo del biglietto è di: ${provisionalPrice.toFixed(2)}€`)
